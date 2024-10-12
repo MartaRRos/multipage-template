c3da1e9796e7911a1585fef2f1864ae52123795f
Marta Rodríguez <martarodriguezros24@alumnostrazos.net>
2024-10-12 21:11:39 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { resolve } from 'path';
+import copy from 'rollup-plugin-copy';
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
-import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   plugins: [
@@ -11,21 +10,12 @@ export default defineConfig({
       include: ['**/*.png', '**/*.jpg', '**/*.jpeg']
     }),
     copy({
-      targets: [{ src: 'src/assets/images/*.svg', dest: 'docs./assets' }],
+      targets: [{ src: 'src/assets/images/*.svg', dest: 'docs/assets' }],
       hook: 'writeBundle' // Aseguramos que los archivos se copien después de que el bundle se haya generado
-    }]
+    })
   ],
-  base: '',
   root: 'src',
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-        about: resolve(__dirname, 'src/html/about.html'),
-        contact: resolve(__dirname, 'src/html/contact.html')
-        // Añade aquí el resto de páginas que quieras.(nombre único: resolve(__dirname, 'src/html/archivo.html'))
-      }
-    },
     outDir: '../docs'
   }
 });
